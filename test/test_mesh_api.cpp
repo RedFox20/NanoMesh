d2678d15ad2464044bae4af510be6585838a07af
Jorma <jorma.rebane@gmail.com>
2018-08-24 17:03:29 +0300

    Disable FBX test if FBX is not supported.

--- test/test_mesh_api.cpp
+++ test/test_mesh_api.cpp
@@ -6,7 +6,30 @@ TestImpl(test_mesh_api)
     TestInit(test_mesh_api)
     {
     }
-    TestCase(basic_api)
+
+    TestCase(load_and_save_obj)
     {
+        Nano::Mesh mesh { "head_male.obj", Nano::Options::LogGroups };
+        AssertThat(mesh.NumGroups(), 7);
+        mesh.SaveAs("head_male.saved.obj", Nano::Options::LogGroups);
+    }
+
+    TestCase(reload_saved_obj)
+    {
+    }
+
+    TestCase(load_save_fbx)
+    {
+        if (!Nano::Mesh::IsFBXSupported())
+            return;
+        Nano::Mesh mesh { "head_male.fbx", Nano::Options::LogGroups };
+        AssertThat(mesh.NumGroups(), 1);
+        mesh.SaveAs("head_male.saved.fbx", Nano::Options::LogGroups);
+    }
+
+    TestCase(force_single_group)
+    {
+        Nano::Mesh mesh { "head_male.obj", Nano::Options::LogGroups|Nano::Options::SingleGroup };
+        AssertThat(mesh.NumGroups(), 1);
     }
 };
--- test/test_mesh_loader_api.cpp
+++ test/test_mesh_loader_api.cpp
@@ -7,30 +7,4 @@ TestImpl(test_mesh_loader_api)
     TestInit(test_mesh_loader_api)
     {
     }
-
-    TestCase(load_and_save_obj)
-    {
-        Nano::Mesh mesh { "head_male.obj", Nano::Options::LogGroups };
-        AssertThat(mesh.NumGroups(), 7);
-        mesh.SaveAs("head_male.saved.obj", Nano::Options::LogGroups);
-    }
-
-    TestCase(reload_saved_obj)
-    {
-
-    }
-
-
-    TestCase(load_save_fbx)
-    {
-        Nano::Mesh mesh { "head_male.fbx", Nano::Options::LogGroups };
-        AssertThat(mesh.NumGroups(), 1);
-        mesh.SaveAs("head_male.saved.fbx", Nano::Options::LogGroups);
-    }
-
-    TestCase(force_single_group)
-    {
-        Nano::Mesh mesh { "head_male.obj", Nano::Options::LogGroups|Nano::Options::SingleGroup };
-        AssertThat(mesh.NumGroups(), 1);
-    }
 };
